a054682d97ec68fa864ff73c68f584a529d4cd4d
Sivani Saravanan <31069721+inavis@users.noreply.github.com>
2021-11-20 14:01:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,259 @@
+
+
+
+
+
+
+let form = document.createElement("form");
+form.setAttribute("action","#");
+form.setAttribute("method","post");
+document.body.appendChild(form);
+
+
+
+//////////////////////////////////////////////////////////////////////
+
+let d1= document.createElement("div");
+d1.setAttribute("class","flex");
+form.appendChild(d1);
+
+let l1 = document.createElement("label");
+l1.setAttribute("for","fname");
+let t1 = document.createTextNode("First Name : ")
+l1.appendChild(t1);
+d1.appendChild(l1);
+
+
+
+let f1=document.createElement("input");
+f1.setAttribute("type","text");
+f1.setAttribute("id","fname");
+d1.appendChild(f1);
+
+
+////////////////////////////////////////////////////////////////////////
+let br1 = document.createElement("br");
+form.appendChild(br1);
+////////////////////////////////////////////////////////////////////////
+
+let d2= document.createElement("div");
+d2.setAttribute("class","flex");
+form.appendChild(d2);
+
+let l2 = document.createElement("label");
+l2.setAttribute("for","lname");
+let t2 = document.createTextNode("Last Name : ")
+l2.appendChild(t2);
+d2.appendChild(l2);
+
+let f2=document.createElement("input");
+f2.setAttribute("type","text");
+f2.setAttribute("id","lname");
+d2.appendChild(f2);
+
+/////////////////////////////////////////////////////////////////////////
+let br2 = document.createElement("br");
+form.appendChild(br2);
+
+
+////////////////////////////////////////////////////////////////////////
+
+let d3= document.createElement("div");
+d3.setAttribute("class","flex");
+form.appendChild(d3);
+
+let l3 = document.createElement("label");
+l3.setAttribute("for","address");
+let t3 = document.createTextNode("Address : ")
+l3.appendChild(t3);
+d3.appendChild(l3);
+
+let f3=document.createElement("input");
+f3.setAttribute("type","text");
+f3.setAttribute("id","address");
+d3.appendChild(f3);
+
+let l4 = document.createElement("label");
+l4.setAttribute("for","pincode");
+let t4 = document.createTextNode("Pincode : ")
+l4.appendChild(t4);
+d3.appendChild(l4);
+
+let f4=document.createElement("input");
+f4.setAttribute("type","text");
+f4.setAttribute("id","pincode");
+d3.appendChild(f4);
+
+////////////////////////////////////////////////////////////////////////////
+let br3 = document.createElement("br");
+form.appendChild(br3);
+
+////////////////////////////////////////////////////////////////////////////
+
+let d4= document.createElement("div");
+d4.setAttribute("class","flex");
+form.appendChild(d4);
+
+let l5 = document.createElement("label");
+l5.setAttribute("for","");
+let t5 = document.createTextNode("Gender : ")
+l5.appendChild(t5);
+d4.appendChild(l5);
+
+let select = document.createElement("select");
+
+let option2= document.createElement("option");
+option2.innerHTML="Male";
+select.appendChild(option2);
+let option3= document.createElement("option");
+option3.innerHTML="Female";
+select.appendChild(option3);
+let option4= document.createElement("option");
+option4.innerHTML="Others";
+select.appendChild(option4);
+let option1= document.createElement("option");
+option1.innerHTML="Don't want to disclose";
+// option1.setAttribute("disabled","disabled");
+// option1.setAttribute("selected","true");
+select.appendChild(option1);
+
+
+d4.appendChild(select);
+
+//////////////////////////////////////////////////////////////////////////////
+
+let br4 = document.createElement("br");
+form.appendChild(br4);
+
+//////////////////////////////////////////////////////////////////////////////
+
+let d5= document.createElement("div");
+d5.setAttribute("class","flexr");
+form.appendChild(d5);
+
+let l6 = document.createElement("label");
+l6.setAttribute("for","");
+let t6 = document.createTextNode("Choice of food: ")
+l6.appendChild(t6);
+d5.appendChild(l6);
+
+
+let din1= document.createElement("div");
+d5.appendChild(din1);
+let c1 =document.createElement("input");
+c1.setAttribute("type","checkbox");
+c1.id="food1"
+din1.appendChild(c1);
+// let lin1 = document.createElement("label");
+// lin1.setAttribute("for","food1")
+// lin1.id="lin1";
+// lin1.innerHTML="continental";
+// din1.appendChild(lin1);
+din1.innerHTML+="continental";
+
+let din2= document.createElement("div");
+d5.appendChild(din2);
+let c2 =document.createElement("input");
+c2.setAttribute("type","checkbox");
+c2.id="food2"
+din2.appendChild(c2);
+// let lin2 = document.createElement("label");
+// lin2.setAttribute("for","food2")
+// lin2.innerHTML="Asian";
+// lin2.id="lin1";
+// din2.appendChild(lin2);
+din2.innerHTML+="Asian"
+
+
+let din3= document.createElement("div");
+d5.appendChild(din3);
+let c3 =document.createElement("input");
+c3.setAttribute("type","checkbox");
+c3.id="food3"
+din3.appendChild(c3);
+// let lin3 = document.createElement("label");
+// lin3.setAttribute("for","food3")
+// lin3.innerHTML="Vegan";
+// lin3.id="lin1";
+// din3.appendChild(lin3);
+din3.innerHTML+="Vegan";
+
+let din4= document.createElement("div");
+d5.appendChild(din4);
+let c4 =document.createElement("input");
+c4.setAttribute("type","checkbox");
+c4.id="food4"
+din4.appendChild(c4);
+// let lin4 = document.createElement("label");
+// lin4.setAttribute("for","food4")
+// lin4.innerHTML="Keto";
+// lin4.id="lin1";
+// din4.appendChild(lin4);
+din4.innerHTML+="Keto";
+
+let din5= document.createElement("div");
+d5.appendChild(din5);
+let c5 =document.createElement("input");
+c5.setAttribute("type","checkbox");
+c5.id="food5"
+din5.appendChild(c5);
+// let lin5 = document.createElement("label");
+// lin5.setAttribute("for","food5")
+// lin5.innerHTML="Others";
+// lin5.id="lin1";
+// din5.appendChild(lin5);
+din5.innerHTML+="Others"
+
+///////////////////////////////////////////////////////////////////////////
+
+let br5 = document.createElement("br");
+form.appendChild(br5);
+
+//////////////////////////////////////////////////////////////////////////////
+
+let d6= document.createElement("div");
+d6.setAttribute("class","flex");
+form.appendChild(d6);
+
+let l7 = document.createElement("label");
+l7.setAttribute("for","state");
+let t7 = document.createTextNode("State :  ")
+l7.appendChild(t7);
+d6.appendChild(l7);
+
+let f5=document.createElement("input");
+f5.setAttribute("type","text");
+f5.setAttribute("id","state");
+d6.appendChild(f5);
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+let br6 = document.createElement("br");
+form.appendChild(br6);
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+let d7= document.createElement("div");
+d7.setAttribute("class","flex");
+form.appendChild(d7);
+
+let l8 = document.createElement("label");
+l8.setAttribute("for","country");
+let t8 = document.createTextNode("Country :  ")
+l8.appendChild(t8);
+d7.appendChild(l8);
+
+let f6=document.createElement("input");
+f6.setAttribute("type","text");
+f6.setAttribute("id","country");
+d7.appendChild(f6);
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+let br7 = document.createElement("br");
+form.appendChild(br7);
+
+//////////////////////////////////////////////////////////////////////////////
